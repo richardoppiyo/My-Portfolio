c8c6a9d9c3266b3a214d0aadb92eb7c78c84ae31
Richard Opiyo <romenyo@dewcis.com>
2022-01-25 18:46:24 +0300

    Error fix according to linter report

--- functions.js
+++ functions.js
@@ -1,9 +1,28 @@
-const btn = document.querySelector(".humb");
-const navLink = document.querySelector("#app-header-menu");
+const btn = document.querySelector('.humb');
+const navLink = document.querySelector('#app-header-menu');
 
-const closeButton = document.querySelector("#humb-cancelor");
+const closeButton = document.querySelector('#humb-cancelor');
 
-const toclose = document.querySelectorAll(".nav-link");
+const toclose = document.querySelectorAll('.nav-link');
+
+
+
+function display() {
+  navLink.classList.add('app-header-menu-visible');
+  const menu = document.querySelector('#welcome');
+  menu.classList.add('welcome-invisible');
+  btn.classList.add('humb-invisible');
+  closeButton.classList.add('humb-cancel-visible');
+}
+
+function close() {
+  navLink.classList.remove('app-header-menu-visible');
+  const menu = document.querySelector('#welcome');
+  menu.classList.remove('welcome-invisible');
+  btn.classList.remove('humb-invisible');
+  closeButton.classList.remove('humb-cancel-visible');
+}
+
 
 btn.addEventListener("click", function () {
   display();
@@ -15,19 +34,3 @@ closeButton.addEventListener("click", function () {
 toclose.forEach((item) => {
   item.addEventListener("click", close);
 });
-
-function display() {
-  navLink.classList.add("app-header-menu-visible");
-  const menu = document.querySelector("#welcome");
-  menu.classList.add("welcome-invisible");
-  btn.classList.add("humb-invisible");
-  closeButton.classList.add("humb-cancel-visible");
-}
-
-function close() {
-  navLink.classList.remove("app-header-menu-visible");
-  const menu = document.querySelector("#welcome");
-  menu.classList.remove("welcome-invisible");
-  btn.classList.remove("humb-invisible");
-  closeButton.classList.remove("humb-cancel-visible");
-}
