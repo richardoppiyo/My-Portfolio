7dce90141707b301364136a5df69541ae794ecd5
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-22 20:12:20 +0300

    add new rpojects

--- functions.js
+++ functions.js
@@ -37,37 +37,22 @@ toclose.forEach((item) => {
 const projectList = [
   {
     id: 1,
-    title: 'Awesome Travels Summit Website ',
-    name: 'Awesome Travels',
-    description: 'A website',
-    detail: 'The Awesome Travel Summit Website give ingormation about the yearly travel agencies summit that hold in different parts of the World. The website is built purely on HTML, CSS AND Vanila JavaScript ',
-    image: './images/awesometravels.png',
+    title: 'Book a lawyer\s seervices App ',
+    name: 'The Lawyer',
+    description: 'A React and Rails web app',
+    detail: 'This is an implemetation os a web application for bookin the services of a lawyer. The front end is built on react/redux and backend on Rails.',
+    image: './images/screenshot.png',
     technologies: {
-      tech1: 'HTML',
-      tech2: 'CSS',
+      tech1: 'Ruby on rails',
+      tech2: 'PLSQL',
       tech3: 'JavaScript',
+      tech4: 'React/Redux',
     },
-    livelink: 'https://richardoppiyo.github.io/awesome-travels/',
-    sourcelink: 'https://github.com/richardoppiyo/awesome-travels',
+    livelink: 'https://book-a-lawyer-app.herokuapp.com',
+    sourcelink: 'https://github.com/richardoppiyo/book-a-lawyer-app-frontend',
   },
   {
     id: 2,
-    title: 'Webpack To Do List',
-    name: 'A To Do List',
-    description: 'Web Application',
-    detail: 'This is a simple web application built with webpack bundler that allows a user to add to dos in their schedule. they are able to mark them as complete and finnaly delete them at the end of a given period. The project is heavily powere by  JavaScript',
-    image: './images/to-do-list.png',
-    technologies: {
-      tech1: 'Webpack bundler',
-      tech2: 'HTML',
-      tech3: 'CSS',
-      tech4: 'JavaScript',
-    },
-    livelink: 'https://richardoppiyo.github.io/To_do_list_webpack/dist/',
-    sourcelink: 'https://github.com/richardoppiyo/To_do_list_webpack',
-  },
-  {
-    id: 3,
     title: 'University of East African Baraton information Mangement System',
     name: 'University of Baraton',
     description: 'Management System',
@@ -81,6 +66,21 @@ const projectList = [
     },
     livelink: 'https://demo.dewcis.com/ueab/',
     sourcelink: 'https://github.com/microverse-students/palindrome-richardoppiyo',
+  },
+  {
+    id: 3,
+    title: 'Awesome Travels Summit Website ',
+    name: 'Awesome Travels',
+    description: 'Travel Sumit Website',
+    detail: 'The Awesome Travel Summit Website give ingormation about the yearly travel agencies summit that hold in different parts of the World. The website is built purely on HTML, CSS AND Vanila JavaScript ',
+    image: './images/awesometravels.png',
+    technologies: {
+      tech1: 'HTML',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+    },
+    livelink: 'https://richardoppiyo.github.io/awesome-travels/',
+    sourcelink: 'https://github.com/richardoppiyo/awesome-travels',
   },
   {
     id: 4,
@@ -100,6 +100,22 @@ const projectList = [
   },
   {
     id: 5,
+    title: 'Webpack To Do List',
+    name: 'A To Do List',
+    description: 'Web Application',
+    detail: 'This is a simple web application built with webpack bundler that allows a user to add to dos in their schedule. they are able to mark them as complete and finnaly delete them at the end of a given period. The project is heavily powere by  JavaScript',
+    image: './images/to-do-list.png',
+    technologies: {
+      tech1: 'Webpack bundler',
+      tech2: 'HTML',
+      tech3: 'CSS',
+      tech4: 'JavaScript',
+    },
+    livelink: 'https://richardoppiyo.github.io/To_do_list_webpack/dist/',
+    sourcelink: 'https://github.com/richardoppiyo/To_do_list_webpack',
+  },
+  {
+    id: 6,
     title: 'Babcock University Information Mangement System',
     name: 'Babcock University',
     description: 'Management System',
@@ -114,23 +130,24 @@ const projectList = [
     },
     livelink: 'https://demo.dewcis.com/babcock/',
     sourcelink: 'https://github.com/microverse-students/palindrome-richardoppiyo',
-  },
-  {
-    id: 6,
-    title: 'ICEA LION insurance Group',
-    name: 'ICEA LION Insurance',
-    description: 'ERP System',
-    detail: 'The Insurance system is an entreprise resource planning system to manage and intergrate the company\'s financial in respect to travel Insurance. ERP system allows users to book and b The Insurance ERP system allows users to book and b ..The Insurance ERP system allows users to book and buy travel insurance online. Administrators are able to see all the insurance bought and approve them as necessary. The system is built on Java, Javascript, Html & CSS and heavily relies on PLSQL functions and triggers for functionalities',
-    image: './images/icelion.png',
-    technologies: {
-      tech1: 'Ruby on rails',
-      tech2: 'PLSQL',
-      tech3: 'JavaScript',
-      tech4: 'Html & CSS',
-    },
-    livelink: 'https://demo.dewcis.com/icealion/icealion',
-    sourcelink: 'https://github.com/microverse-students/palindrome-richardoppiyo',
-  }];
+  }
+  // {
+  //   id: 6,
+  //   title: 'ICEA LION insurance Group',
+  //   name: 'ICEA LION Insurance',
+  //   description: 'ERP System',
+  //   detail: 'The Insurance system is an entreprise resource planning system to manage and intergrate the company\'s financial in respect to travel Insurance. ERP system allows users to book and b The Insurance ERP system allows users to book and b ..The Insurance ERP system allows users to book and buy travel insurance online. Administrators are able to see all the insurance bought and approve them as necessary. The system is built on Java, Javascript, Html & CSS and heavily relies on PLSQL functions and triggers for functionalities',
+  //   image: './images/icelion.png',
+  //   technologies: {
+  //     tech1: 'Ruby on rails',
+  //     tech2: 'PLSQL',
+  //     tech3: 'JavaScript',
+  //     tech4: 'Html & CSS',
+  //   },
+  //   livelink: 'https://demo.dewcis.com/icealion/icealion',
+  //   sourcelink: 'https://github.com/microverse-students/palindrome-richardoppiyo',
+  // }
+];
 
 const containerPopUp = document.querySelector('#mobile-pop-up');
 
